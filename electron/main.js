dbe6a186bcb3da56e2dbb289961e9fbdeb2dd403
sayornotdo <jcskye07@hotmail.com>
2022-12-08 01:16:43 +0800

    icarus electron project initialize complete

---
+++ electron/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require("path");
+
+const mode = "development";
+
+Menu.setApplicationMenu(null);
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: true,
+        contextIsolation: false,
+        nodeIntegration: true,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        }
+    });
+
+    win.loadURL(mode === "development" ? "http://localhost:5173" : `file://${path.join(__dirname, '../dist/index.html')}`)
+
+    if (mode === "development") {
+        win.webContents.openDevTools();
+    }
+};
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    })
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+})
