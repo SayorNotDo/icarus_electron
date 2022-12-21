094b5a7eb2bb3c63f9f66c5ce7fd9a5cd1d10c8e
sayornotdo <jcskye07@hotmail.com>
2022-12-21 16:48:40 +0800

    resource page initialize

---
+++ src/components/TabCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <a-card style="width: 100%" title="Resource" :tab-list="ResourceList" :active-tab-key="key"
+        @tabChange="key => onTabChange(key, 'key')">
+        <template #customTab="item">
+            <span v-if="item.key === 'case'">
+                {{ item.key }}
+            </span>
+        </template>
+    </a-card>
+</template>
+<script>
+import { HomeOutlined } from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    components: {
+        HomeOutlined,
+    },
+    setup() {
+        const ResourceList = [{
+            key: 'case',
+            tab: 'case'
+        }]
+        const ContentList = [{
+            case: 'content'
+        }]
+        const key = ref('case')
+        const onTabChange = (value, type) => {
+            console.log(value, type);
+            if (type === 'key') {
+                key.value = value;
+            }
+        }
+        return {
+            ResourceList,
+            ContentList,
+            key,
+            onTabChange,
+        }
+    }
+})
+</script>
--- src/views/resource.vue
+++ src/views/resource.vue
@@ -1,5 +1,20 @@
 <template>
-    <div></div>
+    <div>
+        <a-row>
+            <TabCardVue />
+        </a-row>
+    </div>
 </template>
 <script>
+import TabCardVue from '../components/TabCard.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    components: {
+        TabCardVue
+    },
+    setup() {
+        return {}
+    }
+})
 </script>
